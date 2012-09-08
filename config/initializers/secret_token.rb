29cffc61d47a71fb7c17b4ea40e2685bb351c5cc
ubuntu <lhok@ubuntu.(none)>
2012-09-08 05:33:41 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CabiDemo1::Application.config.secret_token = '939e28e11574878c489ebc3d03d0456188ffd7f45ff92e9135d1dd82ede9fb8269ca73c8bfa777d6323d23d044d658bc4b4947267e4e7666ec1940c5031810dd'
